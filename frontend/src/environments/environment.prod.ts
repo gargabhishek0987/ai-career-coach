61ea928833d6337fb5e0370804103fa35f0228a0
gargabhishek0987 <gargabhishek742@gmial.com>
2026-06-16 11:24:52 +0530

    [Abhishek garg]add. the production file for deployment

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://ai-career-coach-kzyw.onrender.com/api',
+  googleOAuthUrl: 'https://ai-career-coach-kzyw.onrender.com/oauth2/authorization/google',
+  githubOAuthUrl: 'https://ai-career-coach-kzyw.onrender.com/oauth2/authorization/github'
+};
